bfb47ec15717096cc1c84a6b735d8af7f0d0d6ad
M-Lobos <manuel.lobos.88@gmail.com>
2026-05-27 15:20:58 -0400

    ft: branch update

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,29 @@
 import express from 'express';
-
+import subjectRouter from './routes/subjects.routes'
+import cors from 'cors'
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT;
 
-//middleware
+//cors middleware (mandar a una carpeta de middlewares)
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}))
+
+//middleware for json forms and multiformat
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/subjects', subjectRouter)
 
 app.get('/', (req, res) => {
     res.send('Welcome, API running')
 })
 
+//mandar a services
 app.listen(PORT, () => {
     console.log(`server running at http://localhost:${PORT}`);
 });
+
---
+++ src/routes/subjects.routes.ts
@@ -0,0 +1,79 @@
+import express from 'express';
+import { departments, subjects } from '../services/db/schemas';
+import { and, desc, eq, getTableColumns, ilike, or, sql } from 'drizzle-orm';
+import { db } from '../services/db';
+
+const router = express.Router();
+
+//GET ALL ROUTES
+router.get("/", async (req, res) => {
+    try {
+
+        //here the props for the front for filtering where be processed. Drestructure them from query
+        const { search, department, page = 1, limit = 10 } = req.query;
+
+        const curretPage = Math.max(1, + page);
+        const limitPerPage = Math.max(1, +limit);
+        // How many records to skip to get to the next page
+        const offset = (curretPage - 1) * limitPerPage;
+
+        //array to store the filtering conditions, empty by default.
+        const filterConditions = [];
+
+        // If a search query exists, filte by name OR subject code
+        if (search) {
+            filterConditions.push(
+                or(
+                    ilike(subjects.name, `%${search}%`),
+                    ilike(subjects.code, `%${search}%`),
+                )
+            );
+        }
+
+        //same as above for departments
+        if (department) {
+            filterConditions.push(ilike(departments.name, `%${department}%`))
+        }
+
+        //combine filters using AND if any exisits => whereClause
+        const whereClause = filterConditions.length > 0 ? and(...filterConditions) : undefined;
+
+        const countResult = await db
+            .select({ count: sql<number>`count(*)` })
+            .from(subjects)
+            .leftJoin(departments, eq(subjects.departmentId, departments.id)) //returns all from the left table matching the rows from the right one
+            .where(whereClause);
+
+        const totalCount = countResult[0]?.count ?? 0;
+
+        //Query the data
+        const subjectlist = await db
+            .select({
+                ...getTableColumns(subjects),
+                department: { ...getTableColumns(departments) }
+            }).from(subjects).leftJoin(departments, eq(subjects.departmentId, departments.id))
+            .where(whereClause)
+            .orderBy(desc(subjects.createdAt))
+            .limit(limitPerPage)
+            .offset(offset);
+
+        //return res
+        res.status(200).json({
+            data: subjectlist,
+            pagination: {
+                page: curretPage,
+                limit: limitPerPage,
+                total: totalCount,
+                totalPages: Math.ceil(totalCount / limitPerPage)
+            }
+        })
+
+    } catch (error) {
+        console.error(`GET /subjects error: ${error}`)
+        res.status(500).json({
+            error: 'Fail to get subjets'
+        })
+    }
+})
+
+export default router
