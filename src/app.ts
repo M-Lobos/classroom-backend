de6827775ea73139744dd41fd6101f492f671bc1
M-Lobos <manuel.lobos.88@gmail.com>
2026-06-08 18:23:01 -0400

    ft: Security middleware from arcjet implemented

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express from 'express';
 import subjectRouter from './routes/subjects.routes'
 import cors from 'cors'
+import securityMiddleware from './middlewares/security';
 
 const app = express();
 const PORT = process.env.PORT;
 
 //cors middleware (mandar a una carpeta de middlewares)
-
 if (!process.env.FRONTEND_URL) {
     throw new Error('FRONTEND_URL is NOT set in the .env file');
 }
-
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -20,6 +19,8 @@ app.use(cors({
 //middleware for json forms and multiformat
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(securityMiddleware);
 
 app.use('/api/subjects', subjectRouter)
 
