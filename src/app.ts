b95383cd23e3d51b9f657e3dc024e7f8f45a7473
M-Lobos <manuel.lobos.88@gmail.com>
2026-06-29 16:01:13 -0400

    ft:minor change

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ if (!process.env.FRONTEND_URL) {
 }
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3003',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
