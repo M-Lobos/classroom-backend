027dd9b058958cbba984c81e2e1a4e6c2af64151
M-Lobos <manuel.lobos.88@gmail.com>
2026-06-29 15:36:06 -0400

    ft: modificatin on max rate limit

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ if (!process.env.FRONTEND_URL) {
 }
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || 'http://localhost:3003',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
